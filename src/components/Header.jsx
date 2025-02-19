88aa677efda292499a63437a9f54926518251320
Nuthan Kishore <nuthankishore@hotmail.com>
2025-02-19 15:27:11 +0530

    cloudchef

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import reactLogo from '../assets/cloudchef_transparent.png';
+
+export default function Header() {
+    return (
+        <header>
+            <img src={reactLogo}/>
+            <h1>Clould Chef</h1>
+        </header>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,78 @@
+
+import React from "react"
+export default function Main() {
+//     const [ingredients,setIngredients] = React.useState([])
+    
+//     const ingredientsListItems = ingredients.map(ingredient => (
+//         <li key={ingredient}>{ingredient}</li>
+//     ))
+
+//     function handleSubmit(event) {
+//         event.preventDefault()
+//         const formData = new FormData(event.currentTarget);
+//         const newIngredient = formData.get("ingredient");
+//         setIngredients(prevIngredients =>[...ingredients,newIngredient])
+        
+//     }
+// return (
+//     <main>
+//     <form onClick={handleSubmit} className="add-ingredient-form">
+//         <input 
+//             type="text"
+//             placeholder="e.g. oregano"
+//             aria-label="Add ingredient"
+//             name="ingredient"
+//         />
+//         <button>Add ingredient</button>
+//     </form>
+//     <div className="ingredients-container">
+//     <h4>Added Ingredients</h4>
+//     <ul className="ingredient-list">
+//         {ingredientsListItems}
+//     </ul>
+//     </div>
+// </main>
+const [ingredients, setIngredients] = React.useState([])
+
+const ingredientsListItems = ingredients.map(ingredient => (
+    <li key={ingredient}>{ingredient}</li>
+))
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const newIngredient = formData.get("ingredient")
+    setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+}
+
+return (
+    <main>
+        <form onSubmit={handleSubmit} className="add-ingredient-form">
+            <input
+                type="text"
+                placeholder="e.g. oregano"
+                aria-label="Add ingredient"
+                name="ingredient"
+            />
+            <button>Add ingredient</button>
+        </form>
+        {ingredients.length > 0 && 
+        
+        <section>
+            <div className="ingredients-container"></div>
+                <h2>List of Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                {
+                ingredients.length > 3 && <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+               }
+            </section>
+        }
+    </main>
+)   
+}
